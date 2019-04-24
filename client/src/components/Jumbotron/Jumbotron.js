18c6dbb244cf7d8eee72f8bc4c2b2ad592570c31
Blue Raptor <ciscoaravena@gmail.com>
2019-04-23 23:18:57 -0600

    added background, changed folder sctructure, added css

--- client/src/components/Jumbotron/Jumbotron.js
+++ client/src/components/Jumbotron/Jumbotron.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import '../../css/App.css'
 import Jumbotron from 'react-bootstrap/Jumbotron'
 import Container from 'react-bootstrap/Container'
 
@@ -6,14 +7,14 @@ export default class JumbotronTitle extends Component {
   render() {
     return (
       <div>
-        <Jumbotron fluid>
-  <Container>
-    <h1>Welcome!</h1>
-    <p>
-      Don't know what to make for dinner? Don't have a lot of time? Let ShelfChef do the work for you.
-    </p>
-  </Container>
-</Jumbotron>
+        <Jumbotron fluid id ="fluid">
+          <Container id="container">
+            {/* <h1>Welcome!</h1>
+            <p>
+              Don't know what to make for dinner? Don't have a lot of time? Let ShelfChef do the work for you.
+            </p> */}
+          </Container>
+        </Jumbotron>
       </div>
     )
   }
