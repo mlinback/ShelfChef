ee84b46e35b0f997f6b0fb20c2296fb4f9c8efa9
unknown <mlinback83@gmail.com>
2019-04-27 00:26:11 -0600

    auth components

---
+++ client/src/components/pages/Main.js
@@ -0,0 +1,23 @@
+// import React, { Component } from "react";
+
+// export default class Main extends Component {
+//     render() {
+//         return (
+//             <div>
+//                 <p className="App-intro">
+//                     Hello, {this.props.name}<br />
+//                     Do you want to see the secret area? <a href="/secret">Click here</a>
+//                 </p>
+
+//                 {!this.props.auth.isAuthenticated() &&
+//                 <div>
+//                     <hr/>
+//                     Please login first
+//                     <hr/>
+//                     <button onClick={this.props.auth.login}>Login</button>
+//                 </div>
+//                 }
+//             </div>
+//         )
+//     }
+// }
--- client/src/index.js
+++ client/src/index.js
@@ -10,3 +10,35 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+// Auth0 Code:
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import App from './App';
+// import registerServiceWorker from './registerServiceWorker';
+// import Auth from "./Auth";
+
+// const auth = new Auth();
+
+// let state = {};
+// window.setState = (changes) => {
+//   state = Object.assign({}, state, changes);
+
+// ReactDOM.render(<App {...state} />, document.getElementById('root'));
+
+// };
+
+// /*eslint no-restricted-globals: 0*/
+// let username = auth.getProfile().given_name || "Marissa";
+
+// let initialState = {
+//   name: username,
+//   location: location.pathname.replace(/^\/?|\/$/g, ""),
+//   auth
+// };
+
+// window.setState(initialState);
+
+// registerServiceWorker();
